43e1549d82c7b95489ef4b964b2b4320ad766e35
przemek <Brodeon@gmail.com>
2023-01-26 21:30:43 +0100

    Initial Commit

---
+++ CustomAlert/CustomAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomAlertApp.swift
+//  CustomAlert
+//
+//  Created by Przemek on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
